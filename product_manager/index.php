850140b494710321477cb7e43bac4f32e91e45cb
ben <bencowan@mycwi.cc>
2018-09-19 11:24:02 -0600

    Initial deploy to GH.

---
+++ product_manager/index.php
@@ -0,0 +1,101 @@
+<?php
+require('../model/database.php');
+require('../model/category.php');
+require('../model/category_db.php');
+require('../model/product.php');
+require('../model/product_db.php');
+	//include the fields and validate classes
+require_once('../model/fields.php');
+require_once('../model/validate.php');
+
+$validate = new Validate();
+$fields = $validate->getFields();
+$fields->addField('code', '');
+$fields->addField('name', '');
+$fields->addField('price');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_products';
+    }
+}
+
+if ($action == 'list_products') {
+    // Get the current category ID
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+
+    // Get product and category data
+    $current_category = CategoryDB::getCategory($category_id);
+    $categories = CategoryDB::getCategories();
+    $products = ProductDB::getProductsByCategory($category_id);
+
+    // Display the product list
+    include('product_list.php');
+} else if ($action == 'delete_product') {
+    // Get the IDs
+    $product_id = filter_input(INPUT_POST, 'product_id', 
+            FILTER_VALIDATE_INT);
+    $category_id = filter_input(INPUT_POST, 'category_id', 
+            FILTER_VALIDATE_INT);
+
+    // Delete the product
+    ProductDB::deleteProduct($product_id);
+
+    // Display the Product List page for the current category
+    header("Location: .?category_id=$category_id");
+} else if ($action == 'show_add_form') {
+    $categories = CategoryDB::getCategories();
+    include('product_add.php');
+	 
+} else if ($action == 'add_product') {
+    $category_id = filter_input(INPUT_POST, 'category_id', 
+            FILTER_VALIDATE_INT);
+    $code = filter_input(INPUT_POST, 'code');
+    $name = filter_input(INPUT_POST, 'name');
+    $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+	 
+		//validate the fields
+	$validate->text('code', $code, true, 1, 11);
+	$validate->text('name', $name);
+	$validate->number('price', $price);
+		
+		/*
+		       $validate->email('email', $email);
+        $validate->password('password', $password);
+        $validate->verify('verify', $password, $verify);
+		  */
+	 
+	 
+		// Load appropriate view based on hasErrors
+	if ($fields->hasErrors()) {
+		$categories = CategoryDB::getCategories();
+		include('product_add.php');
+	} else {
+        $current_category = CategoryDB::getCategory($category_id);
+        $product = new Product($current_category, $code, $name, $price);
+        ProductDB::addProduct($product);
+
+			// Display the Product List page for the current category
+        header("Location: .?category_id=$category_id");
+	}
+	/*
+    if ($category_id == NULL || $category_id == FALSE || $code == NULL || 
+            $name == NULL || $price == NULL || $price == FALSE) {
+        $error = "Invalid product data. Check all fields and try again.";
+        include('../errors/error.php');
+    } else {
+        $current_category = CategoryDB::getCategory($category_id);
+        $product = new Product($current_category, $code, $name, $price);
+        ProductDB::addProduct($product);
+
+        // Display the Product List page for the current category
+        header("Location: .?category_id=$category_id");
+	*/
+}
+?>
